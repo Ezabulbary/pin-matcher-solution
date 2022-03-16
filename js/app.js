cab2fd6d8e5ee05247799106f1db64d19e407750
Ezabulbary <ezabul.bary@gmail.com>
2022-03-16 12:53:31 +0600

    pin macher done

--- js/app.js
+++ js/app.js
@@ -12,4 +12,35 @@ function getPin(){
 function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
-}
+};
+
+document.getElementById('key-pad').addEventListener('click', function(evant){
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if(isNaN(number)){
+        if(number == 'C'){
+            calcInput.value = '';
+        }
+    }
+    else{
+        const previusNumber = calcInput.value;
+        const newNumber = previusNumber + number;
+        calcInput.value = newNumber;
+    }
+});
+
+function varifyPin(){
+    const pin = document.getElementById('display-pin').value;
+    const typedNumber = document.getElementById('typed-numbers').value;
+    const failError = document.getElementById('notify-fail');
+    const successMassage = document.getElementById('notify-success');
+
+    if(pin == typedNumber){
+        successMassage.style.display = 'block';
+        failError.style.display = 'none';
+    }
+    else{
+        failError.style.display = 'block';
+        successMassage.style.display = 'none';
+    }
+};
